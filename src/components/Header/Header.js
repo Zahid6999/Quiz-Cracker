a505ba76743d51bbf4041b5175baff303c758add
habllu <zahidhasan6999@gmail.com>
2022-10-11 20:26:37 +0600

    basic Change && Components|| layout added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+          <div>
+            <h1>Game Of Code Quiz</h1>
+          </div>
+
+
+          <nav>
+            <Link to=''></Link>
+            <Link to=''></Link>
+            <Link to=''></Link>
+            <Link to=''></Link>
+          </nav>
+        </div>
+    );
+};
+
+export default Header;
