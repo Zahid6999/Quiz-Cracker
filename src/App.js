89986e3356365c5f8e6864620e8ded97c8eb0345
habllu <zahidhasan6999@gmail.com>
2022-10-12 19:40:36 +0600

    added Quiz Card and quiz display || mobile responsive

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Blogs from './components/Blogs/Blogs';
 import Home from './components/Home/Home';
+import QuizDetails from './components/QuizDateils/QuizDateils';
 import Topics from './components/Topics/Topics';
 import Main from './layouts/Main';
 
@@ -13,12 +14,20 @@ function App() {
     {path: '/',
     element: <Main></Main>,
     children: [
-      {path: '/', element: <Home></Home>},
+      {path: '/', element: <Home></Home>, loader: () => fetch('https://openapi.programming-hero.com/api/quiz')},
       {path: '/blogs', element: <Blogs></Blogs>},
-      {path: '/topics', element: <Topics></Topics>}
+      {path: '/topics', element: <Topics></Topics>},
+      {path: '/quiz/:id', element: <QuizDetails></QuizDetails>,
+       loader: async({params}) => {
+        return fetch(`https://openapi.programming-hero.com/api/quiz/${params.id}`)
+       }
+    }
     ]
-  }
+  },
 
+
+
+  {path: '/*', element: <p>404 </p>}
   ]);
 
 
---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Quiz = ({quizData}) => {
+const {id, name, logo} = quizData;
+    return (
+        <div>
+          <div className="card w-80 bg-indigo-100 shadow-xl mx-auto">
+            <figure><img className='h-72' src={logo} alt="Technology" /></figure>
+            <div className="card-body">
+                <h2 className="text-3xl font-semibold">{name}</h2>
+                <p><span className='text-lg font-semibold'>tryAgain attempt <br /></span><span className='text-green-400 text-lg font-semibold'>success  will come</span></p>
+                <div className="card-actions justify-end">
+                <Link to={`/quiz/${id}`}><button className="btn btn-primary">Practice Now</button></Link>
+                </div>
+            </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Quiz;
